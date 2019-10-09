ad53dd398cb32145afb565702b0a0efdce3d30d2
Ayesha Jownally <sheik.bibi.ayesha.siddiqa.jownally@elca.mu>
2019-10-09 20:28:21 +0400

    fix: nav - stick border top on current page

--- turbo/src/app/nav/nav.component.ts
+++ turbo/src/app/nav/nav.component.ts
@@ -12,6 +12,15 @@ export class NavComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var myNav = document.getElementById("myNavbar");
+    var menu = myNav.getElementsByClassName("nav-link");
+    for (var i = 0; i < menu.length; i++) {
+      menu[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+      });
+    }
   }
 
 }
