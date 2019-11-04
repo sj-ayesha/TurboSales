d3bb8256fc2bcb838fe0ac5878a4e22d0ec9a894
Ayesha Jownally <sheik.bibi.ayesha.siddiqa.jownally@elca.mu>
2019-11-04 10:53:33 +0400

    fix: car list filter

--- turbo/src/app/shopcars/car-list/car-list.component.ts
+++ turbo/src/app/shopcars/car-list/car-list.component.ts
@@ -11,29 +11,29 @@ import { OrderPipe } from 'ngx-order-pipe';
 export class CarListComponent implements OnInit, OnChanges {
   
   actualPage: number = 1;
-  noAvailaibilityMsg: boolean = false;
   order: string = 'title';
   reverse: boolean = false;
   sortedCollection: any[];
   displayMode: number = 1;
+  numberOfItems: string = "6";
 
   @Input() currentFilter: any;
 
   // arr: any[] = [];
   cardetails: any[];
   details: {
-    id: number;
-    img: string;
-    title: string;
-    series: string;
-    stockNum: string;
-    Transmission: string;
-    fuel: string;
-    engine: string;
-    year: string;
-    price: number;
-    bodyType: string;
-    miles: number;
+    // id: number;
+    // img: string;
+    // title: string;
+    // series: string;
+    // stockNum: string;
+    // Transmission: string;
+    // fuel: string;
+    // engine: string;
+    // year: string;
+    // price: number;
+    // bodyType: string;
+    // miles: number;
   }[];
 
   constructor(private router: Router, private carService: CarService, private orderPipe: OrderPipe) {
@@ -68,12 +68,12 @@ export class CarListComponent implements OnInit, OnChanges {
         this.cardetails = this.cardetails.filter((carDetail) => {
           if (priceRange.length && carDetail.price >= minPrice && carDetail.price <= maxPrice) {
             this.scrollToTop();
-            this.noAvailaibilityMsg = false;
+            // this.noAvailaibilityMsg = false;
             return carDetail;
           }
-          else {
-            this.noAvailaibilityMsg = true;
-          }
+          // else {
+          //   this.noAvailaibilityMsg = true;
+          // }
         }) || [];
       }
 
@@ -89,12 +89,12 @@ export class CarListComponent implements OnInit, OnChanges {
         this.cardetails = this.cardetails.filter((cardetail) => {
           if (arrayYears.includes(cardetail.year)) {
             this.scrollToTop();
-            this.noAvailaibilityMsg = false;
+            // this.noAvailaibilityMsg = false;
             return cardetail;
           }
-          else {
-            this.noAvailaibilityMsg = true;
-          }
+          // else {
+          //   this.noAvailaibilityMsg = true;
+          // }
         });
       }
 
@@ -107,12 +107,12 @@ export class CarListComponent implements OnInit, OnChanges {
         this.cardetails = this.cardetails.filter((cardetail) => {
           if (arrBrandNames.includes(cardetail.title.toLowerCase())) {
             this.scrollToTop();
-            this.noAvailaibilityMsg = false;
+            // this.noAvailaibilityMsg = false;
             return cardetail;
           }
-          else {
-            this.noAvailaibilityMsg = true;
-          }
+          // else {
+          //   this.noAvailaibilityMsg = true;
+          // }
         });
       }
 
@@ -125,17 +125,21 @@ export class CarListComponent implements OnInit, OnChanges {
         this.cardetails = this.cardetails.filter((cardetail) => {
           if (arrBodyTypeNames.includes(cardetail.bodyType.toLowerCase())) {
             this.scrollToTop();
-            this.noAvailaibilityMsg = false;
+            // this.noAvailaibilityMsg = false;
             return cardetail;
           }
-          else {
-            this.noAvailaibilityMsg = true;
-          }
+          // else {
+          //   this.noAvailaibilityMsg = true;
+          // }
         });
       }
 
     }
 
+  }
+
+  onAdd() {
+    this.numberOfItems = (<HTMLInputElement>document.getElementById('numberOfItems')).value;
   }
 
   scrollToTop() {
@@ -211,118 +215,7 @@ export class CarListComponent implements OnInit, OnChanges {
 
 
   initialiseCarDetails() {
-    this.cardetails = [{
-      id: 1,
-      img: '../../assets/img/Cars/mercedes.jpg',
-      title: 'MERCEDES',
-      series: 'Benz E-Class',
-      stockNum: 'MER001',
-      Transmission: 'Automatic',
-      fuel: 'Diesel',
-      engine: 'Turbocharged four-cylinder engine',
-      year: '2019',
-      price: 21500,
-      bodyType: 'Sedans',
-      miles: 1800
-    },
-    {
-      id: 2,
-      img: '../../assets/img/Cars/Lancer.jpg',
-      title: 'Mitsubishi',
-      series: 'Lancer Sedan',
-      stockNum: 'MIT001',
-      Transmission: 'Automatic',
-      fuel: 'Diesel',
-      engine: 'Turbochargedengine',
-      year: '2015',
-      price: 10500,
-      bodyType: 'Sedans',
-      miles: 2000
-    },
-    {
-      id: 3,
-      img: '../../assets/img/Cars/FordMustang.jpg',
-      title: 'Ford',
-      series: 'Mustang Hybrid',
-      stockNum: 'FOR001',
-      Transmission: 'Automatic',
-      fuel: 'Diesel',
-      engine: 'Turbochargedengine',
-      year: '2019',
-      price: 10500,
-      bodyType: 'Coupes',
-      miles: 1500
-    },
-    {
-      id: 4,
-      img: '../../assets/img/Cars/kiaRio.jpg',
-      title: 'Kia',
-      series: 'Rio Hatchback',
-      stockNum: 'KIA001',
-      Transmission: 'Automatic',
-      fuel: 'Diesel',
-      engine: 'Turbochargedengine',
-      year: '2018',
-      price: 10500,
-      bodyType: 'SUVs',
-      miles: 1000
-    },
-    {
-      id: 5,
-      img: '../../assets/img/Cars/audi.jpg',
-      title: 'Audi',
-      series: 'A7',
-      stockNum: 'AUD001',
-      Transmission: 'Automatic',
-      fuel: 'Diesel',
-      engine: 'Turbochargedengine',
-      year: '2019',
-      price: 10500,
-      bodyType: 'Coupes',
-      miles: 1500
-    },
-    {
-      id: 6,
-      img: '../../assets/img/Cars/Vans/Caddy.jpg',
-      title: 'Volkswagen',
-      series: 'Caddy',
-      stockNum: 'VOL001',
-      Transmission: 'Manual',
-      fuel: 'Diesel',
-      engine: 'Turbo',
-      year: '2016',
-      price: 5500,
-      bodyType: 'Vans',
-      miles: 1700
-    },
-    {
-      id: 7,
-      img: '../../assets/img/Cars/Truck/mercedesTruck.jpeg',
-      title: 'Mercedes',
-      series: 'Benz',
-      stockNum: 'MER002',
-      Transmission: 'Manual',
-      fuel: 'Diesel',
-      engine: 'Turbo',
-      year: '2019',
-      price: 555500,
-      bodyType: 'Trucks',
-      miles: 1700
-    },
-    {
-      id: 8,
-      img: '../../assets/img/Cars/Jeep/Jeep.jpg',
-      title: 'Jeep',
-      series: 'Wrangler',
-      stockNum: 'JEE001',
-      Transmission: 'Manual',
-      fuel: 'Petrol',
-      engine: 'Turbo',
-      year: '2019',
-      price: 700000,
-      bodyType: 'Convertibles',
-      miles: 1600
-    }];
+    this.cardetails = this.carService.getCarDetails();
   }
 
 }
--- turbo/src/app/shopcars/filter-cars/filter-cars.component.ts
+++ turbo/src/app/shopcars/filter-cars/filter-cars.component.ts
@@ -10,7 +10,7 @@ import { Subscription } from 'rxjs';
 export class FilterCarsComponent implements OnInit {
 
   private routeSubscription: Subscription;
-  noAvailaibilityMsg: boolean = false;
+  // noAvailaibilityMsgnoAvailaibilityMsg: boolean = false;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
