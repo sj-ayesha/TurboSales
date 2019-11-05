e6c198a549d29f85460afc49436e99bfa7582c70
Ayesha Jownally <sheik.bibi.ayesha.siddiqa.jownally@elca.mu>
2019-11-05 10:53:01 +0400

    fix: validation checkout

--- turbo/src/app/components/checkout/checkout.component.ts
+++ turbo/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../../_entities/item.entity';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -8,15 +9,59 @@ import { Item } from '../../_entities/item.entity';
 })
 export class CheckoutComponent implements OnInit {
 
-  private items: Item[] = [];
+  	private items: Item[] = [];
 	private total: number = 0;
 	private totalQuantity: number = 0;
+	billingForm: FormGroup;
+	submitted = false;
+	error: string;
+	showMsg: boolean = false;
+	namePattern = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$";
+	emailPattern = "[^@]+@[^\.]+\..+";
+	zipcode = "^\d{5}(?:[-\s]\d{4})?$";
+	creditCardNum = "4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11}";
+	expiryDate = "^\d{2}\/\d{2}$";
+	cvv = "/^[0-9]{3,4}$/";
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.loadCart();
+	this.loadCart();
+	this.billingForm = this.formBuilder.group({
+		firstName: ['', [Validators.required, Validators.pattern(this.namePattern)]],
+		lastName: ['', [Validators.required, Validators.pattern(this.namePattern)]],
+		emailAdd: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+		city: ['', [Validators.required, Validators.pattern(this.namePattern)]],
+		state: ['', [Validators.required, Validators.pattern(this.namePattern)]],
+		zipcode: ['', [Validators.required, Validators.pattern(this.zipcode)]],
+		cardname: ['', Validators.required],
+		cardnumber: ['', [Validators.required, Validators.pattern(this.creditCardNum)]],
+		expdate: ['', [Validators.required, Validators.pattern(this.expiryDate)]],
+		cvv: ['', [Validators.required, Validators.pattern(this.cvv)]]
+	  });
   }
+
+  
+  get f() { return this.billingForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.billingForm.invalid) {
+		console.log("invalid");
+      return;
+    }
+    else {
+      this.showMsg = true;
+    }
+
+    setTimeout(() => {
+      this.showMsg = false;
+      this.submitted = false;
+    }, 2000);
+  }
+
 
   loadCart(): void {
 		this.total = 0;
